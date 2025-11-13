49c82071b8c8d89688ea0b51f20f3363f620d9e6
Bonsai-Tr33 <moritz.langer@stud-mail.uni-wuerzburg.de>
2025-11-13 11:03:48 +0100

    continued on Evaluation and added Subsection to Tex

--- 41/Evaluation/3111.py
+++ 41/Evaluation/3111.py
@@ -1,2 +1,11 @@
 # measuring the speed of light
 
+# dependencies
+import numpy as np
+import os
+import pandas as pd
+import statistics
+from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+from pathlib import Path
+
