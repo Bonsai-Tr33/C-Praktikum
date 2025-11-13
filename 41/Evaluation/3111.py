21441f112fda8b8153eaa3816391c926c8d7e743
Hunnesx <winklerhannes@web.de>
2025-11-13 15:23:38 +0100

    continued 3111.py

--- 41/Evaluation/3111.py
+++ 41/Evaluation/3111.py
@@ -12,11 +12,19 @@ from pathlib import Path
 def c(x,t):
     return  2* (x-0.2315) / t
 
+def dc(x, t, dx, dt):
+    return np.sqrt((2/t*dx)**2+(2(x-0.2315)/(t**2)*dt)**2)
+
 Orte = [3.91, 4.02, 4.14, 7.92, 8.03, 8.15, 11.712, 11.822, 11.942]
 Zeit = [24*10**(-9), 24*10**(-9), 28*10**(-9), 50*10**(-9), 52*10**(-9), 58*10**(-9), 78*10**(-9), 80*10**(-9), 86*10**(-9)]
+dx = 0.0005
+dt = 5*10**(-9)
 
-Bip = []
+Bip = [[], []]
 for i in range(len(Orte)):
-    Bip.append(c(Orte[i], Zeit[i]))
+    Bip.append([c(Orte[i], Zeit[i]), dc(Orte[i], Zeit[i], dx, dt)])
+
+
+
 
 print(sum(Bip)/len(Bip))
