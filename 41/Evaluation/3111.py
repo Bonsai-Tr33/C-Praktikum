4bf63a4e48e2d5de082b94e2e5e84308a5a061d7
Hunnesx <winklerhannes@web.de>
2025-11-13 15:08:00 +0100

    3111

--- 41/Evaluation/3111.py
+++ 41/Evaluation/3111.py
@@ -9,3 +9,14 @@ from matplotlib import pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 from pathlib import Path
 
+def c(x,t):
+    return  2* (x-0.2315) / t
+
+Orte = [3.91, 4.02, 4.14, 7.92, 8.03, 8.15, 11.712, 11.822, 11.942]
+Zeit = [24*10**(-9), 24*10**(-9), 28*10**(-9), 50*10**(-9), 52*10**(-9), 58*10**(-9), 78*10**(-9), 80*10**(-9), 86*10**(-9)]
+
+Bip = []
+for i in range(len(Orte)):
+    Bip.append(c(Orte[i], Zeit[i]))
+
+print(Bip)
