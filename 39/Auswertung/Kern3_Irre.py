3993285190353984c4ee1a179bb49829e4834008
Bonsai-Tr33 <moritz.langer@stud-mail.uni-wuerzburg.de>
2026-03-25 10:52:14 +0100

    auswertung Hysterese

--- 39/Auswertung/Kern3_Irre.py
+++ 39/Auswertung/Kern3_Irre.py
@@ -58,6 +58,16 @@ def Br(H, B, H_Tolerance=0.5):
 
     return B_R, D_BR
 
+def hysteresis_area(H, B):
+    H = np.asarray(H)
+    B = np.asarray(B)
+
+    # numerisches Linienintegral
+    area = np.trapz(B, H)
+
+    return abs(area)
+
+
 # ---------
 # Data Path initialization
 base_path = Path.cwd() / '39'
@@ -66,21 +76,32 @@ img_path = base_path / 'Images'
 
 # ---------
 # First measurements
-messreihe3 = Messreihe(data_path / 'Ringkern3Irre_39.csv', N1=1010, N2=53, R=67.3, F=1)
+messreihe3 = Messreihe(data_path / 'Ringkern3Irre_39.csv', N1=1010, N2=53, R=0.2, F=1)
 Data3 = messreihe3.dataArray()
 
 # shift correction
-B_Shift = symmetric(Data3.iloc[:, 1], Data3.iloc[:,2], symPX=58)
-DataH = Data3.iloc[:, 1] 
+B_Shift = symmetric(Data3.iloc[:, 1], Data3.iloc[:,2], symPX=6000)
+DataH = Data3.iloc[:, 1]*10**(-3) 
 DataB = Data3.iloc[:,2] - B_Shift
 print('Symmetrisierungsfaktor Ringkern 1: ' + str(B_Shift) + 'T')
+
+# koerzitivfeldstärke measurement 3
+HC3, DHC3 = HC(DataH, DataB, B_Tolerance=0.008)
+print('Koerzitivfeldstärke Ringkern 3: (' + str(HC3) + ' ± ' + str(DHC3) + ') A/m')
+
+# remanenz measurement 3
+BR3, DBR3 = Br(DataH, DataB, H_Tolerance=0.009)
+print('Remanenz Ringkern 3: (' + str(BR3) + ' ± ' + str(DBR3) + ') T')
+
 
 
 fig, plt3 = plt.subplots(figsize=(8,5))
 plt3.scatter(DataH, DataB, label='Messpunkte', c='black', s=0.5, zorder=4)
 plt3.axvline(x=0, color='grey', linestyle='--', zorder=1, alpha=0.5)
 plt3.axhline(y=0, color='grey', linestyle='--', zorder=1, alpha=0.5)
-plt3.set_xlabel('H [A/m]')
+plt.scatter(HC3, 0, marker='x', color='red', label='H_C', zorder=5)
+plt.scatter(0, BR3, marker='x', color='orange', label='B_r', zorder=5)
+plt3.set_xlabel('H [kA/m]')
 plt3.set_ylabel(ylabel='B [T]')
 plt3.tick_params(axis='both', which='minor', direction='in', right=True, top=True)
 plt3.tick_params(axis='both', which='major', direction='in', right=True, top=True, length=5)
@@ -89,8 +110,8 @@ plt3.legend()
 plt3in = inset_axes(plt3, width='40%', height='40%', loc='lower right',bbox_to_anchor=(0, 0.08, 1, 1), bbox_transform=plt3.transAxes)
 plt3in.scatter(DataH, DataB, c='black', marker='.', s=1)
 plt3in.annotate('',
-    xy=(3.496, 1.07),     # Pfeilspitze (H, B)
-    xytext=(4.164, 1.129), # Pfeilanfang (H, B)
+    xy=(1300*10**(-3), 1.05),     # Pfeilspitze (H, B)
+    xytext=(1450*10**(-3), 1.09), # Pfeilanfang (H, B)
     arrowprops=dict(
         arrowstyle='->',
         color='red',
@@ -100,8 +121,8 @@ plt3in.annotate('',
 )
 plt3in.annotate(
     '',
-    xy=(4.346, 0.981),     # Pfeilspitze (H, B)
-    xytext=(3.865, 0.965), # Pfeilanfang (H, B)
+    xy=(1533*10**(-3), 0.9987),     # Pfeilspitze (H, B)
+    xytext=(1303*10**(-3), 0.9778), # Pfeilanfang (H, B)
     arrowprops=dict(
         arrowstyle='->',
         color='red',
@@ -111,8 +132,8 @@ plt3in.annotate(
 )
 plt3in.annotate(
     '',
-    xy=(4.954, 1.083),     # Pfeilspitze (H, B)
-    xytext=(5.387, 1.094), # Pfeilanfang (H, B)
+    xy=(1633*10**(-3), 1.0567),     # Pfeilspitze (H, B)
+    xytext=(1774*10**(-3), 1.0662), # Pfeilanfang (H, B)
     arrowprops=dict(
         arrowstyle='->',
         color='blue',
@@ -122,8 +143,8 @@ plt3in.annotate(
 )
 plt3in.annotate(
     '',
-    xy=(2.512, 0.9449),     # Pfeilspitze (H, B)
-    xytext=(2.927, 0.995), # Pfeilanfang (H, B)
+    xy=(994.5*10**(-3), 0.9641),     # Pfeilspitze (H, B)
+    xytext=(1083*10**(-3), 0.9948), # Pfeilanfang (H, B)
     arrowprops=dict(
         arrowstyle='->',
         color='blue',
@@ -131,8 +152,14 @@ plt3in.annotate(
     ),
     zorder=10
 )
-plt3in.set_xlim(2,6.5)
-plt3in.set_ylim(0.8, 1.2)
+plt3in.set_xlim(900*10**(-3),1900*10**(-3))
+plt3in.set_ylim(0.95, 1.1)
 mark_inset(plt3, plt3in, loc1=1, loc2=2, lw=1)
 plt.savefig(img_path / 'Hyster3_irre.png')
 plt.show()
+
+
+# numeric integral
+A = hysteresis_area(DataH, DataB)
+print('Flächeninhalt der Hysteresekurve: ' + str(A))
+print('Ummagnetisierungsverluste: ' + str(A/(7.8*10**3)) + ' J/kg')
